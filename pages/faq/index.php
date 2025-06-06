4c642a530f53e44c42ff3a42176415f4cde6b6e4
Vladlen Schetinin <vvschetinin@gmail.com>
2025-06-06 15:53:29 +0300

    Начало создания программной структуры

--- pages/faq/index.php
+++ pages/faq/index.php
@@ -1,8 +1,10 @@
+<?header('Content-type: text/html; charset=utf-8');?>
+<?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
+<?php require_once $root . '/interface/init.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 
 <head>
-  <meta charset="UTF-8" />
   <link rel="icon" type="image/svg+xml" href="/assets/images/vite.svg" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Вопросы</title>
@@ -350,162 +352,21 @@
       </section>
     </main>
 
-    <footer class="footer">
-      <div class="container footer-top">
-        <div class="row">
-          <div class="col-100 col-md-50 content-left">
-            <a class="footer-logo svg" href="/">
-              <svg class="logo-svg" viewBox="0 0 230 161" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M26.6051 120.606L32.5848 112.814C30.5049 111.255 28.5117 109.524 26.6485 107.619C19.2389 100.216 14.1259 91.1255 11.6127 81.2554L2.59985 85.4978C6.58629 99.6536 15.1658 111.905 26.6051 120.606Z"
-                  fill="url(#paint0_linear)" />
-                <path
-                  d="M69.3726 130.909L68.4627 121.125C65.8628 121.385 63.2196 121.428 60.5765 121.342C50.0904 120.909 40.1676 117.706 31.6315 112.121L27.8617 121.342C40.4276 129.047 55.0735 132.294 69.3726 130.909Z"
-                  fill="url(#paint1_linear)" />
-                <path
-                  d="M0 67.0997C0 73.4633 0.909947 79.6105 2.55652 85.4546L11.5693 81.2122C10.3994 76.6668 9.8361 71.9049 9.8361 67.0997C9.8361 58.0953 11.916 49.394 15.8157 41.5585L7.23625 36.7966C2.59985 45.8875 0 56.1906 0 67.0997Z"
-                  fill="url(#paint2_linear)" />
-                <path
-                  d="M7.23627 36.7964L15.8158 41.5583C18.5456 36.1038 22.1854 31.0389 26.6051 26.58C31.6315 21.5583 37.4379 17.5756 43.7642 14.7618L38.6078 6.36353C25.0452 12.7272 13.9959 23.4631 7.23627 36.7964Z"
-                  fill="url(#paint3_linear)" />
-                <path
-                  d="M67.1628 0C56.9367 0 47.274 2.29437 38.6078 6.36364L43.7642 14.7619C51.0437 11.5152 58.9733 9.82684 67.1628 9.82684C73.7058 9.82684 80.0754 10.9091 86.0984 12.987L89.2182 3.67965C82.2853 1.2987 74.8757 0 67.1628 0Z"
-                  fill="url(#paint4_linear)" />
-                <path
-                  d="M89.2181 3.7229L86.0983 12.987C94.1579 15.8008 101.524 20.3896 107.72 26.58C110.364 29.2207 112.703 32.0779 114.74 35.1082L122.756 29.4805C114.74 17.619 102.997 8.48481 89.2181 3.7229Z"
-                  fill="url(#paint5_linear)" />
-                <path
-                  d="M134.326 67.0995C134.326 53.1601 130.079 40.2164 122.756 29.4805L114.74 35.1082C121.066 44.5021 124.489 55.5411 124.489 67.0995H134.326Z"
-                  fill="url(#paint6_linear)" />
-                <path
-                  d="M87.3983 13.2468C90.1264 13.2468 92.338 11.0373 92.338 8.31177C92.338 5.58621 90.1264 3.37671 87.3983 3.37671C84.6701 3.37671 82.4586 5.58621 82.4586 8.31177C82.4586 11.0373 84.6701 13.2468 87.3983 13.2468Z"
-                  fill="#E84F1C" />
-                <path
-                  d="M118.726 37.2727C121.455 37.2727 123.666 35.0632 123.666 32.3377C123.666 29.6121 121.455 27.4026 118.726 27.4026C115.998 27.4026 113.787 29.6121 113.787 32.3377C113.787 35.0632 115.998 37.2727 118.726 37.2727Z"
-                  fill="#DE0030" />
-                <path
-                  d="M129.386 72.0347C132.114 72.0347 134.326 69.8252 134.326 67.0996C134.326 64.3741 132.114 62.1646 129.386 62.1646C126.658 62.1646 124.446 64.3741 124.446 67.0996C124.446 69.8252 126.658 72.0347 129.386 72.0347Z"
-                  fill="#7D051F" />
-                <path
-                  d="M40.861 15.7144C43.5891 15.7144 45.8007 13.5049 45.8007 10.7793C45.8007 8.05374 43.5891 5.84424 40.861 5.84424C38.1329 5.84424 35.9213 8.05374 35.9213 10.7793C35.9213 13.5049 38.1329 15.7144 40.861 15.7144Z"
-                  fill="#009E00" />
-                <path
-                  d="M11.266 44.8052C13.9941 44.8052 16.2057 42.5957 16.2057 39.8701C16.2057 37.1446 13.9941 34.9351 11.266 34.9351C8.53788 34.9351 6.32629 37.1446 6.32629 39.8701C6.32629 42.5957 8.53788 44.8052 11.266 44.8052Z"
-                  fill="#1CD4BD" />
-                <path
-                  d="M7.06288 88.1384C9.79101 88.1384 12.0026 85.9289 12.0026 83.2034C12.0026 80.4778 9.79101 78.2683 7.06288 78.2683C4.33475 78.2683 2.12317 80.4778 2.12317 83.2034C2.12317 85.9289 4.33475 88.1384 7.06288 88.1384Z"
-                  fill="#0054E8" />
-                <path
-                  d="M29.7249 121.688C32.4531 121.688 34.6646 119.479 34.6646 116.753C34.6646 114.028 32.4531 111.818 29.7249 111.818C26.9968 111.818 24.7852 114.028 24.7852 116.753C24.7852 119.479 26.9968 121.688 29.7249 121.688Z"
-                  fill="#362196" />
-                <path
-                  d="M80.5087 79.1774L69.936 63.593C69.156 62.2077 67.6828 61.2554 65.9495 61.2554C63.4363 61.2554 61.3998 63.29 61.3998 65.8008C61.3998 67.4458 62.2664 68.8744 63.5663 69.6536L79.3388 80.303C80.1187 80.8225 81.0287 79.9567 80.5087 79.1774Z"
-                  fill="white" />
-                <defs>
-                  <lineargradient x1="6.04283" y1="84.1438" x2="28.5536" y2="118.823" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#0054E8" stop-opacity="0.8" />
-                    <stop offset="1" stop-color="#0054E8" stop-opacity="0" />
-                  </lineargradient>
-                  <lineargradient x1="29.5338" y1="118.018" x2="69.611" y2="128.277" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#362196" stop-opacity="0.8" />
-                    <stop offset="1" stop-color="#362196" stop-opacity="0" />
-                  </lineargradient>
-                  <lineargradient x1="8.84954" y1="38.8607" x2="5.91492" y2="83.5082" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#1CD4BD" stop-opacity="0.8" />
-                    <stop offset="1" stop-color="#1CD4BD" stop-opacity="0" />
-                  </lineargradient>
-                  <lineargradient x1="39.4887" y1="9.39708" x2="11.4865" y2="37.6217" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#009E00" stop-opacity="0.8" />
-                    <stop offset="1" stop-color="#009E00" stop-opacity="0" />
-                  </lineargradient>
-                  <lineargradient x1="87.7165" y1="7.38467" x2="41.6552" y2="7.38467" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#E84F1C" stop-opacity="0.8" />
-                    <stop offset="1" stop-color="#E84F1C" stop-opacity="0" />
-                  </lineargradient>
-                  <lineargradient x1="119.886" y1="30.8421" x2="89.3295" y2="7.90342" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#DE0030" stop-opacity="0.8" />
-                    <stop offset="1" stop-color="#DE0030" stop-opacity="0" />
-                  </lineargradient>
-                  <lineargradient x1="130.778" y1="66.2393" x2="119.82" y2="31.3772" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#7D051F" stop-opacity="0.8" />
-                    <stop offset="1" stop-color="#7D051F" stop-opacity="0" />
-                  </lineargradient>
-                </defs>
-                <text class="svg-title" x="140" y="124" fill="white" text-anchor="middle">AI Tools</text>
-              </svg>
-            </a>
-            <ul class="footer-address">
-              <li>Московская область</li>
-              <li>г. Ивантеевка</li>
-              <li class="visible hidden-md"><a href="tel:+79260522029">+7 (926) 052-20-29</a></li>
-              <li class="visible hidden-md"><a href="mailto:info@vschetinin.ru">info@vschetinin.ru</a></li>
-              <li class="mb-0 mb-2--sm mb-0--md fs-20 font-middle">Мост к успеху вашего бизнеса</li>
-            </ul>
-          </div>
-          <div class="col-100 col-md-50 content-right"></div>
-        </div>
-      </div>
-      <div class="footer-copyright text-center">&copy; Vladlen Schetinin 2025<span>All Rights Reserved</span></div>
-    </footer>
+    <!-- ================================================== -->
+
+    <?php if (file_exists($root . "/template/shared/footer.php")): ?>
+    <?php require_once $root . '/template/shared/footer.php';?>
+    <?php endif;?>
+
   </div>
 
-  <div class="fixed-contact">
-    <ul class="fixed-contact-body">
-      <li>
-        <a href="tel:+79260522029" class="contact-item-link">
-          <i class="icon-fixed-contact">
-            <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-              <path class="icon-fill"
-                d="M19.092 14.535c-.81-.814-3.237-2.442-4.74-2.442-.346 0-.693.116-.924.233-.81.465-1.387.813-1.618.813-.116 0-.347-.116-.694-.465H11c-1.04-.93-1.271-1.162-1.618-1.511l-.115-.117c-.116-.116-.116-.232-.232-.232-.346-.349-.578-.698-1.502-1.628-.463-.581-.694-.93-.81-1.163 0-.232.232-.581.694-1.279.694-.814.694-1.86.116-3.023-.463-.93-1.156-1.86-1.85-2.674V.93C5.105.35 4.412 0 3.718 0c-.81 0-1.503.465-1.85.698h-.115C1.059 1.163.48 1.86.25 2.558-.097 3.605-.328 5 1.29 7.791c1.388 2.442 2.544 4.07 4.509 6.046 1.85 1.86 2.658 2.442 4.392 3.837C12.156 19.186 14.121 20 15.51 20c1.271 0 2.196 0 3.699-1.744 1.387-1.744.693-2.79-.116-3.721Z"
-                fill="#050029" />
-            </svg>
-          </i>
-          <span class="contact-item-text">+7 (926) 052-20-29</span>
-        </a>
-      </li>
-      <li>
-        <a href="mailto:born-1965@mail.ru" class="contact-item-link">
-          <i class="icon-fixed-contact">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" style="enable-background: new 0 0 20 20"
-              xml:space="preserve">
-              <path class="icon-fill"
-                d="M10 10.8c-1.6 0-10-6.1-10-6.1V4c0-.8.8-1.5 1.7-1.5h16.7c.8 0 1.6.7 1.6 1.5v.8s-8.3 6-10 6zm0 2c1.8 0 10-5.8 10-5.8v9c0 .8-.8 1.5-1.7 1.5H1.7C.8 17.5 0 16.8 0 16V7s8.4 5.8 10 5.8z"
-                fill-rule="evenodd" clip-rule="evenodd" fill="#050029" />
-            </svg>
-          </i>
-          <span class="contact-item-text">born-1965@mail.ru</span>
-        </a>
-      </li>
-      <li>
-        <a href="" class="contact-item-link">
-          <i class="icon-fixed-contact">
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M23.1117 4.49449C23.4296 2.94472 21.9074 1.65683 20.4317 2.227L2.3425 9.21601C0.694517 9.85273 0.621087 12.1572 2.22518 12.8975L6.1645 14.7157L8.03849 21.2746C8.13583 21.6153 8.40618 21.8791 8.74917 21.968C9.09216 22.0568 9.45658 21.9576 9.70712 21.707L12.5938 18.8203L16.6375 21.8531C17.8113 22.7334 19.5019 22.0922 19.7967 20.6549L23.1117 4.49449ZM3.0633 11.0816L21.1525 4.0926L17.8375 20.2531L13.1 16.6999C12.7019 16.4013 12.1448 16.4409 11.7929 16.7928L10.5565 18.0292L10.928 15.9861L18.2071 8.70703C18.5614 8.35278 18.5988 7.79106 18.2947 7.39293C17.9906 6.99479 17.4389 6.88312 17.0039 7.13168L6.95124 12.876L3.0633 11.0816ZM8.17695 14.4791L8.78333 16.6015L9.01614 15.321C9.05253 15.1209 9.14908 14.9366 9.29291 14.7928L11.5128 12.573L8.17695 14.4791Z"
-                fill="#0F0F0F" />
-            </svg>
-          </i>
-          <span class="contact-item-text">@VladlenGross</span>
-        </a>
-      </li>
-    </ul>
-  </div>
+  <?php if (file_exists($root . "/template/forms/fixedcontact.php")): ?>
+  <?php require_once $root . '/template/forms/fixedcontact.php';?>
+  <?php endif;?>
 
-  <div class="start-project-overlay">
-    <div class="wrapper">
-      <button class="close-btn display-block js-closebutton">
-        <span></span>
-        <span></span>
-      </button>
-      <div class="container--middle inner">
-        <div class="row justify-center">
-          <div class="col-100 col-xxl-90">
-            <h2 class="h1-inner deco-line lh-1 color-black">Начать проект</h2>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <?php if (file_exists($root . "/template/forms/mainform.php")): ?>
+  <?php require_once $root . '/template/forms/mainform.php';?>
+  <?php endif;?>
 
 </body>
 
